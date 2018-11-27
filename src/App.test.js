6100a96966098a3b4e9efd0133af1c2663304303
bajdcc <bajdcc@foxmail.com>
2018-11-27 09:44:39 +0800

    Add appveyor

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from "./store/store";
+import {Provider} from "react-redux";
+import {Layout} from "antd";
+const {Content} = Layout;
+
+/* Absolute root of the app */
+class RootComponent extends React.Component {
+  render() {
+    return (
+      <Provider store={this.props.store}>
+        <Layout>
+          <Content>
+            <App/>
+          </Content>
+        </Layout>
+      </Provider>
+    );
+  }
+}
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    ReactDOM.render(
+      <RootComponent store={store}/>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
